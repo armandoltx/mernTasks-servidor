405593942c94aec450edca4cb916415682df57d8
Armando Ruiz <armando.ruiz@industrie.co>
2020-09-28 13:53:03 +1000

    231 Conectando MongoDB a la app

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // crear el servidor
 const app = express();
 
+// Conectar a la base de datos
+conectarDB();
+
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
-// // definir la pag pprincipal
+// // definir la pag principal
 // app.get('/', (req, res) => {
 //   res.send('Hola Mundo')
 // });
