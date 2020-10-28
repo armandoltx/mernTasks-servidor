d54b2496524f8491bcf778c3eccb569aec2abce2
Armando Ruiz <armando.ruiz@industrie.co>
2020-10-29 09:18:09 +1100

    235 Agregando el controlador para crear nuevos usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,11 @@
 // Rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
 
 // Crea un usuario
 // endpoint => api/usuarios
-router.post('/', () => {});
+router.post('/',
+  usuarioController.crearUsuario
+);
 module.exports = router;
