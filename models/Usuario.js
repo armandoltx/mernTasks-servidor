55361e4c54fe512994bca0decb80d60bdc7760af
Armando Ruiz <armando.ruiz@industrie.co>
2021-02-11 10:16:06 +1100

    237 Definiendo el Modelo de Usuarios con Mongoose

---
+++ models/Usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UsuariosSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  registro: {
+    type: Date,
+    default: Date.now()
+  },
+});
+
+module.exports = mongoose.moder('Usuario', UsuariosSchema);
+// le decimos a mongoose que vamos a registrar el modelo Usuaio con el schema UsuariosSchema
