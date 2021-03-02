26dd51c6eebb05c3e0f5ae88a648243903908d30
Armando Ruiz <armando.ruiz@industrie.co>
2021-03-03 10:47:30 +1100

    250 Validacion de los proyectos

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 
 // Crea Proyectos
 // endpoint => api/proyectos
 router.post('/',
   auth, // verifica lo q pasa en auth y luego va al siguiente middleware, en este caso el controller
+  [
+    check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+  ],
   proyectoController.crearProyecto
 );
 
